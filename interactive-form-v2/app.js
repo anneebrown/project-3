0b68efc2cbf7494b0851fbb87d84f7af19ed9836
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-03-29 19:23:11 -0700

    only appropriate color options are visible

--- interactive-form-v2/app.js
+++ interactive-form-v2/app.js
@@ -1,3 +1,5 @@
+//I used the Study Guide as a basis
+
 //give focus to the first field
 document.getElementById("name").focus();
 
@@ -18,13 +20,46 @@ selectFields.addEventListener('change', (event) => {
   });
 
 //T-shirt section
+
 //Hide the “Select Theme” `option` element in the “Design” menu.
-let selectThemeOption = document.getElementById('design').firstElementChild;
-selectThemeOption.style.display = 'none';
+let selectThemeOption = document.getElementById('design');
+selectThemeOption.firstElementChild.style.display = 'none';
 
-//Update the “Color” field to read “Please select a T-shirt theme”.
+//Update the “Color” field to read “Please select a T-shirt theme” until a theme is selected
+const colorOptions = document.getElementById('color');
+colorOptions.style.display = 'none';
+let colorLabel = document.getElementById('colors-js-puns').firstElementChild;
+colorLabel.textContent = 'Please select a T-shirt theme';
 
-//Hide the colors in the “Color” drop down menu.
+//add an event listener to listen for changes, use an if clause to determine what to display, change the label to say "Color:"
+// try event.target.value too in the if clause ?
+//
+selectThemeOption.addEventListener('change', (event) => {
+  //gets the index value of the selected field to be able to target it with the if clauses, idea from here: https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/selectedIndex
+  let selectedOption = selectThemeOption.selectedIndex;
+  console.log(selectedOption);
+  colorOptions.style.display = 'block';
+  if (selectedOption == 1){
+     colorLabel.textContent = 'Color: ';
+     colorOptions.innerHTML = `
+     <select id="color">
+     <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+     <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
+     <option value="gold">Gold (JS Puns shirt only)</option> 
+   </select>`
+   
+  } else if (selectedOption == 2){
+   colorLabel.textContent = 'Color: ';
+   colorOptions.innerHTML = `
+   <select id="color">
+              <option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+              <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
+              <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> 
+            </select>`
 
-//add an event listener to listen for changes, use an if clause to determine what to display
+  }
+});
+
+
+//activity section
 
